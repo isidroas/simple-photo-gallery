ab5111da3b7ca40e8259d7d3604eff8daae02696
Isidro Arias <isidroariass@hotmail.es>
2023-01-10 20:56:00 +0100

    overwrite output

--- simplegallery/media.py
+++ simplegallery/media.py
@@ -86,7 +86,7 @@ def create_video_thumbnail(video_path, thumbnail_path, height):
     probe = ffmpeg.probe(video_path)
     video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
     duration = float(video_stream['duration'])
-    ffmpeg.input(video_path, ss=duration/2).filter('scale', -1, height).output(thumbnail_path, vframes=1).run()
+    ffmpeg.input(video_path, ss=duration/2).filter('scale', -1, height).output(thumbnail_path, vframes=1).global_args("-y").run()
 
 
 def create_thumbnail(input_path, thumbnail_path, height):
